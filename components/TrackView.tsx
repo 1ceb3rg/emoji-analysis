84a4549c2374e93f212dc64d3d174ccd07d5ae76
1ceb3rg <62146940+1ceb3rg@users.noreply.github.com>
2021-12-10 22:56:15 +0400

    test full screen

--- components/TrackView.tsx
+++ components/TrackView.tsx
@@ -45,69 +45,68 @@ function TrackView(props: ITrackViewProps) {
   }, [newTrack, resetImageShow]);
 
   return (
-    <div
-      style={{ backgroundColor: bgColor }}
-      className={classNames('absolute inset-0 overflow-hidden', { hidden: !isShowing })}
-    >
-      <div className="w-full flex items-end justify-end">
-        <button type="button" onClick={onClose}>
-          <XIcon className="h-10 w-10" />
-        </button>
-      </div>
-      <div className="grid grid-cols-6 duration-75  h-screen justify-center py-10 rounded-md">
-        <NextTrackButton
-          onClick={() => {
-            setImageShow(false);
-
-            nextTrack('left');
-          }}
-          direction="left"
-        />
-        <div className="col-span-4">
-          <Transition
-            className=" w-full h-full "
-            show={imageShow}
-            appear
-            enter="transition-opacity duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-500"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="flex w-full  flex-col">
-              <div className="basis-2/3 h-2/3 ">
-                <img className="object-contain shadow-md" alt="Album Cover" src={track.album.images[0].url} />
-              </div>
-              <div className={classNames('flex basis-1/3 flex-col mt-3 shadow-md  rounded-xl  ')}>
-                <TrackSection className="bg-gray-200 p-2  " title="Artist">
-                  {track.artists.reduce((artists, artist) => {
-                    return `${artists}, ${artist}`;
-                  })}
-                </TrackSection>
-                <TrackSection className="bg-white p-2 " title="Name">
-                  {track.name}
-                </TrackSection>
-                <TrackSection className="bg-gray-200 p-2 " title="Album">
-                  {track.album.name}
-                </TrackSection>
-
-                <TrackSection className="bg-white  p-2" title="Tempo">
-                  {Math.round(track.tempo)}
-                </TrackSection>
-              </div>
-            </div>
-          </Transition>
+    <div className={classNames('fixed inset-0 overflow-hidden', { hidden: !isShowing })}>
+      <div style={{ backgroundColor: bgColor }} className={classNames('absolute inset-0 overflow-hidden')}>
+        <div className="w-full flex items-end justify-end">
+          <button type="button" onClick={onClose}>
+            <XIcon className="h-10 w-10" />
+          </button>
         </div>
+        <div className="grid grid-cols-6 duration-75  h-screen justify-center py-10 rounded-md">
+          <NextTrackButton
+            onClick={() => {
+              setImageShow(false);
 
-        <NextTrackButton
-          onClick={() => {
-            setImageShow(false);
-            resetImageShow();
-            nextTrack('right');
-          }}
-          direction="right"
-        />
+              nextTrack('left');
+            }}
+            direction="left"
+          />
+          <div className="col-span-4">
+            <Transition
+              className=" w-full h-full "
+              show={imageShow}
+              appear
+              enter="transition-opacity duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="flex w-full  flex-col">
+                <div className="basis-2/3 h-2/3 ">
+                  <img className="object-contain shadow-md" alt="Album Cover" src={track.album.images[0].url} />
+                </div>
+                <div className={classNames('flex basis-1/3 flex-col mt-3 shadow-md  rounded-xl  ')}>
+                  <TrackSection className="bg-gray-200 p-2  " title="Artist">
+                    {track.artists.reduce((artists, artist) => {
+                      return `${artists}, ${artist}`;
+                    })}
+                  </TrackSection>
+                  <TrackSection className="bg-white p-2 " title="Name">
+                    {track.name}
+                  </TrackSection>
+                  <TrackSection className="bg-gray-200 p-2 " title="Album">
+                    {track.album.name}
+                  </TrackSection>
+
+                  <TrackSection className="bg-white  p-2" title="Tempo">
+                    {Math.round(track.tempo)}
+                  </TrackSection>
+                </div>
+              </div>
+            </Transition>
+          </div>
+
+          <NextTrackButton
+            onClick={() => {
+              setImageShow(false);
+              resetImageShow();
+              nextTrack('right');
+            }}
+            direction="right"
+          />
+        </div>
       </div>
     </div>
   );
